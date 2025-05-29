f3cf83c08861f21b0689abfe6c809cef9cb993f0
pOuri_A <pouria7679@gmail.com>
2025-05-29 12:57:26 +0330

    Refactor UserController for MediatR and RESTful routes
    Updated UserController to utilize MediatR for command handling. Changed route attribute to include "api/" prefix and modified HTTP routes to use lowercase and hyphenated naming conventions. Changed UpdateProfile from POST to PUT method to align with RESTful practices.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -7,9 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace InterpriseChatService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class UserController : ControllerBase
-{ 
+{
     private readonly ILogger<UserController> _logger;
     private readonly IMediator _mediator;
 
@@ -18,27 +18,25 @@ public class UserController : ControllerBase
         _logger = logger;
         _mediator = mediator;
     }
-
-    [HttpPost]
-    [Route("Register")]
+     
+    [HttpPost("register")]
     public async Task<ActionResult<ResultDTO<Guid>>> Register([FromBody] RegisterUserCommand command)
     {
         var result = await _mediator.Send(command);
         return Ok(result);
     }
-    [HttpPost]
-    [Route("ChnagePassword")]
-    public async Task<ActionResult<MessageDTO>> ChnagePassword([FromBody] ChangePasswordUserCommand command)
+     
+    [HttpPut("change-password")]
+    public async Task<ActionResult<MessageDTO>> ChangePassword([FromBody] ChangePasswordUserCommand command)
     {
         var result = await _mediator.Send(command);
         return Ok(result);
     }
-    [HttpPost]
-    [Route("UpdateProfile")]
+     
+    [HttpPut("update-profile")]
     public async Task<ActionResult<MessageDTO>> UpdateProfile([FromBody] UpdateProfileUserCommand command)
     {
         var result = await _mediator.Send(command);
         return Ok(result);
     }
-
 }
