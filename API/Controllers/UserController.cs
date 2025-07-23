97d8c582d51fd96339e2604c927b2cee678bb9ee
pOuri_A <pouria7679@gmail.com>
2025-07-23 14:35:05 +0330

    Add GetUserByEmail controller

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Application.Features.UserUseCase;
 using Application.Features.UserUseCase.Commands;
+using Application.Features.UserUseCase.Queries;
 using Domain.Base;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -39,4 +40,24 @@ public class UserController : ControllerBase
         var result = await _mediator.Send(command);
         return Ok(result);
     }
+
+    /// <summary>
+    /// Get a user by email address
+    /// </summary>
+    /// <param name="email">User email address</param>
+    /// <returns>User info</returns>
+    [HttpGet("by-email")]
+    public async Task<IActionResult> GetUserByEmail([FromQuery] string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return BadRequest("Email is required");
+
+        var result = await _mediator.Send(new GetUserByEmailQuery(email));
+
+        if (!result.IsSuccess)
+            return NotFound(result.Message); // or return BadRequest if you want to show validation error
+
+        return Ok(result);
+    }
+
 }
